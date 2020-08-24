373911534c6f02c30b307ce9f783d20031f6ea41
Nathan Burns <nathanandreburns@gmail.com>
2020-08-24 00:46:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,42 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 require_relative './course.rb'
 
 class Scraper
-  
+  def get_page
+    #use Nokogiri and open-uri to get all HTML from webpage
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+
+  end
+
+  def get_courses
+    #use CSS selector to grab all HTML elements that contain a course
+    #return value is a collection of XML elements, each of which describes a course
+    #inspect website to find CSS selector the contains the courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    #instantiates Course objects, giving each one the correct title, schedule, and description
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
+
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -17,6 +49,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.print_courses
 
 
 
